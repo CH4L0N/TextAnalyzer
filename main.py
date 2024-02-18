927390c76b38c8e368491a1718f79def34059a93
CH4L0N <chalon@mailbox.org>
2024-02-18 22:38:51 +0100

    Added documentation; Created task_template.py.

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+
+main.py: Project #1 for ENGETO Certification (Python Data Academy).
+
+author: O. Ch.
+
+email: chalon@mailbox.org
+
+discord: .ch4l0n
+
+
+"""
+
+
 print("Hello World!")
 
 print("Hello Git!")
